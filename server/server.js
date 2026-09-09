84ebb7037ac60d95dff4b029a93a4b384a3e0939
Nandini Sihare <sihare934@gmail.com>
2026-09-09 13:18:23 +0530

    Configure server for production deployment

--- server/server.js
+++ server/server.js
@@ -49,8 +49,14 @@ app.get("/", (req, res) => {
   });
 });
 
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
